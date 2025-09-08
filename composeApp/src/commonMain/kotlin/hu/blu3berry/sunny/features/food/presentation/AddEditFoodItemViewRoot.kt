94f5c37a05860306c04789e276b2022e7b9c4770
gyenese.matyas <gyenese.matyas@novaservices.hu>
2025-09-08 21:15:21 +0200

    Add Status bar padding, to remove clip with top bar.

--- composeApp/src/commonMain/kotlin/hu/blu3berry/sunny/features/food/presentation/AddEditFoodItemViewRoot.kt
+++ composeApp/src/commonMain/kotlin/hu/blu3berry/sunny/features/food/presentation/AddEditFoodItemViewRoot.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
@@ -44,7 +45,9 @@ fun AddEditFoodItemViewRoot(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
-    Scaffold {
+    Scaffold(
+        modifier = Modifier.statusBarsPadding()
+    ) {
         AddEditFoodItemView(
             state = state,
             onAction = viewModel::onAction,
--- composeApp/src/commonMain/kotlin/hu/blu3berry/sunny/features/food/presentation/FoodItemListViewRoot.kt
+++ composeApp/src/commonMain/kotlin/hu/blu3berry/sunny/features/food/presentation/FoodItemListViewRoot.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,6 +24,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults.contentWindowInsets
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -38,7 +42,9 @@ fun FoodItemListViewRoot(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
-    Scaffold {
+    Scaffold(
+        modifier = Modifier.statusBarsPadding()
+    ) {
         FoodItemListView(
             state = state,
             onAction = viewModel::onAction
